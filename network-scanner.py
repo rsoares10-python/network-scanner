3298d9dc753542b4eec7c098141385c072a56173
Rafael <rsoares.monteiro10@gmail.com>
2019-10-08 23:04:55 -0400

    Add IP range option

--- network-scanner.py
+++ network-scanner.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# Built-in/Generic Imports
+import optparse
+
 # Thirdparty imports
 import scapy.all as scapy
 
@@ -30,3 +33,20 @@ def print_result(clients_list):
     print('-------------------------------------------')
     for client in clients_list:
         print(client['ip'] + '\t\t' + client['mac'])
+
+
+def get_args():
+    '''Get user arguments  and parse them.'''
+
+    usage = "usage: %prog [OPTION1] arg1"
+    parser = optparse.OptionParser(usage=usage)
+    parser.add_option("-t", "--target", dest="target", help="Specify IP target range")
+    (options, arguments) = parser.parse_args()
+    if not options.target:
+        parser.error("[-] Please specify an IP target range, use --help for more info")
+    return options
+
+
+options = get_args()
+scan_result = scan(options.target)
+print_result(scan_result)
